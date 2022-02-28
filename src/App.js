0281020a4b0c89f1f781daa9a5edee9cc6683d43
meithnav <meithnavlakha@gmail.com>
2022-02-28 20:37:14 +0530

    Connected to server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
+import react , {useState, useEffect} from "react" 
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+  const [chats , setChats] = useState([]);
+  
+    const fetchChats = async()=>{
+
+    const {data} = await axios.get("/api/chats");
+    console.log(data)
+    setChats(data);
+
+  }
+
+  useEffect(()=>{
+
+    fetchChats();
+    
+  },[])
   return (
     <div className="App">
      <h1 > Hello </h1>
+     {chats.map((c)=>(
+       <h1>{c.chatName}</h1>
+     ))}
     </div>
   );
 }
